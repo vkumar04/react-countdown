637120d53f2047959c95a06fa0c783b65f5445ba
vkumar04 <vick.kumar44@gmail.com>
2017-06-26 19:54:17 -0400

    added placeholder text and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
+// css
+import './App.css';
 
-class App extends component{
+class App extends Component{
   render(){
     return(
-      <div>
-        
+      <div className='App'>
+        <div>Countdown to December 25, 2017</div>
+        <div>
+          <div>14 Days</div>
+          <div>30 Hours</div>
+          <div>15 Minutes</div>
+          <div>30 Seconds</div>
+        </div>
+        <input type='text' placeholder='new date'/>
+        <button>Submit</button>
       </div>
     )
   }
 }
+
+export default App;
