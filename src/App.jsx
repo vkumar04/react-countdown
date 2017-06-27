21255fd198278c0c1ab89e7642f3d33bed303bba
vkumar04 <vick.kumar44@gmail.com>
2017-06-26 20:06:30 -0400

    more styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+
 // css
 import './App.css';
 
@@ -6,12 +7,12 @@ class App extends Component{
   render(){
     return(
       <div className='App'>
-        <div>Countdown to December 25, 2017</div>
+        <div className='App-title'>Countdown to December 25, 2017</div>
         <div>
-          <div>14 Days</div>
-          <div>30 Hours</div>
-          <div>15 Minutes</div>
-          <div>30 Seconds</div>
+          <div className='Clock-days'>14 Days</div>
+          <div className='Clock-hours'>30 Hours</div>
+          <div className='Clock-minutes'>15 Minutes</div>
+          <div className='Clock-seconds'>30 Seconds</div>
         </div>
         <input type='text' placeholder='new date'/>
         <button>Submit</button>
